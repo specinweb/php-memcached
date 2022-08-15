e97d08ccd1d4e85a063d7a8870ad51470c065c9f
i. pralnikov <specinweb@gmail.com>
2022-08-15 18:43:25 +0300

    Change README memcached php

--- Demo.php
+++ Demo.php
@@ -2,27 +2,15 @@
 
 use Memcached\Client;
 
-
-// Create Memcached.php instance ...
-$memcached = new Client(
-    '127.0.0.1'
-);
-
-// Some setup for randomized key(s) for demonstration ...
+$memcached = new Client('127.0.0.1');
 srand(microtime(true));
 $dummy = md5(rand(1111, 9999));
-
-// Try to do some stuff with memcached instance ...
 try {
-
     $memcached->set($dummy, 1);
     $result = $memcached->get($dummy);
-
     $memcached->delete($dummy);
-
 } catch (Exception $e) {
     $result = $e->getMessage();
-
 }
 
 echo '<pre>';
